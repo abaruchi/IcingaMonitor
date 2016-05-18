6e1a90fb743dc5b529198dcfd4ce872181951654
Artur Baruchi <artur.baruchi@gmail.com>
2016-05-18 04:14:30 -0300

    Python script to send file to S3

--- Scripts/Python/uploadS3.py
+++ Scripts/Python/uploadS3.py
@@ -1,11 +1,47 @@
-import tinys3
+import os
 
-access_key_id = "AKIAI2VT7X6ANQ3O73RA"
-access_key = "TCyah0k3ufq9uAMl9bEoROfGIvlgtbWUsVrYWZTm"
+import boto
+from boto.s3.key import Key
 
-conn = tinys3.Connection(access_key_id, access_key, tls=True,
-                         endpoint='s3.amazonaws.com')
 
-f = open('testing_s3.txt','rb')
-print conn.upload('testing_s3.txt',f,'testing-bucket')
-print conn.get('./testing_s3.txt','testing-bucket')
+def upload_to_s3(aws_access_key_id, aws_secret_access_key, file, bucket, key,
+                 callback=None, md5=None, reduced_redundancy=False,
+                 content_type=None):
+
+    try:
+        size = os.fstat(file.fileno()).st_size
+    except:
+        # Not all file objects implement fileno(),
+        # so we fall back on this
+        file.seek(0, os.SEEK_END)
+        size = file.tell()
+
+    conn = boto.connect_s3(aws_access_key_id, aws_secret_access_key)
+    bucket = conn.get_bucket(bucket, validate=True)
+    k = Key(bucket)
+    k.key = key
+    if content_type:
+        k.set_metadata('Content-Type', content_type)
+    sent = k.set_contents_from_file(file, cb=callback, md5=md5,
+                                    reduced_redundancy=reduced_redundancy,
+                                    rewind=True)
+
+    # Rewind for later use
+    file.seek(0)
+
+    if sent == size:
+        return True
+    return False
+
+AWS_ACCESS_KEY = 'AKIAI2VT7X6ANQ3O73RA'
+AWS_ACCESS_SECRET_KEY = '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'
+
+file = open('testing_s3.txt', 'r+')
+
+key = file.name
+bucket = 'icingabucket'
+
+if upload_to_s3(AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY, file, bucket, key):
+    print 'It worked!'
+else:
+    print 'The upload failed...'
