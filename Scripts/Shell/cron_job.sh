cb5f8c73fc36d46b92ec8a33040e02dc1baf2249
Artur Baruchi <artur.baruchi@gmail.com>
2016-05-18 16:13:10 -0300

    Fixes

--- Scripts/Shell/cron_job.sh
+++ Scripts/Shell/cron_job.sh
@@ -2,6 +2,7 @@
 
 # Collects log files from Dockers
 ./copy_docker_files.sh
+././make_backup.sh
 
 # Send file to S3
 /usr/bin/env python log_file_upload.py AKIAJLT2AZFWDHCZYIUA vi7TCLbUTbXjy4rIao7NfdVcuIEMmEA4eYRNf09G
