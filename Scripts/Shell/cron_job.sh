dff71b1afcfb4b54864ae0f7e2e2b529bbcf95ac
Artur Baruchi <artur.baruchi@gmail.com>
2016-05-18 15:39:24 -0300

    Added crontab jobs files.

---
+++ Scripts/Shell/cron_job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Collects log files from Dockers
+./copy_docker_files.sh
+
+# Send file to S3
+/usr/bin/env python log_file_upload.py AKIAJLT2AZFWDHCZYIUA vi7TCLbUTbXjy4rIao7NfdVcuIEMmEA4eYRNf09G
+/usr/bin/env python bkp_file_upload.py AKIAJLT2AZFWDHCZYIUA vi7TCLbUTbXjy4rIao7NfdVcuIEMmEA4eYRNf09G
