2847a3fe967342e58e790ec41130d01624652fec
Artur Baruchi <artur.baruchi@gmail.com>
2016-05-18 23:18:52 -0300

    Fixes in cron_jobs

--- Scripts/Shell/cron_job.sh
+++ Scripts/Shell/cron_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Collects log files from Dockers
-./copy_docker_files.sh
-././make_backup.sh
+/root/copy_docker_files.sh
+/root/make_backup.sh
 
 # Send file to S3
-/usr/bin/env python log_file_upload.py AKIAJLT2AZFWDHCZYIUA vi7TCLbUTbXjy4rIao7NfdVcuIEMmEA4eYRNf09G
-/usr/bin/env python bkp_file_upload.py AKIAJLT2AZFWDHCZYIUA vi7TCLbUTbXjy4rIao7NfdVcuIEMmEA4eYRNf09G
+/usr/bin/env python /root/log_file_upload.py AKIAJLT2AZFWDHCZYIUA vi7TCLbUTbXjy4rIao7NfdVcuIEMmEA4eYRNf09G
+/usr/bin/env python /root/bkp_file_upload.py AKIAJLT2AZFWDHCZYIUA vi7TCLbUTbXjy4rIao7NfdVcuIEMmEA4eYRNf09G
